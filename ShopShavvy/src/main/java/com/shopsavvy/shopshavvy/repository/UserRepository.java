d659234f5baa95063e515d84e582932102ae8e07
manvi <manvi.jain1@tothenew.com>
2025-04-02 23:47:43 +0530

    Added finders method in UserRepository

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/repository/UserRepository.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/repository/UserRepository.java
@@ -2,12 +2,21 @@ package com.shopsavvy.shopshavvy.repository;
 
 import com.shopsavvy.shopshavvy.model.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     public User findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE LOWER(u.companyName) = LOWER(:companyName)")
+    public Boolean existsByCompanyNameIgnoreCase(@Param("companyName") String companyName);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.gst = :gst")
+    public Boolean existsByGst(@Param("gst") String gst);
+
 }
