536db7f0b5fa284be26a3305993be9f61ed35690
manvi <manvi.jain1@tothenew.com>
2025-04-05 03:12:58 +0530

    Added some basic jpql queries

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/repository/AuthTokenRepository.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/repository/AuthTokenRepository.java
@@ -7,10 +7,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthTokenRepository extends JpaRepository<AuthToken, Long> {
 
     @Query("DELETE FROM AuthToken a WHERE a.userEmail = :email")
     @Modifying
     void deleteTokenByEmail(@Param("email") String email);
+
+    Optional<AuthToken> findByToken(String token);
+
+    @Query("DELETE FROM AuthToken a WHERE a.userEmail = :email AND a.tokenType = com.shopsavvy.shopshavvy.model.token.TokenType.ACCESS")
+    @Modifying
+    void deleteAccessTokenByEmail(@Param("email") String email);
+
+    boolean existsByToken(String token);
 }
