243fc30922320ec9194304eac5b15a18dfc12f13
manvi <manvi.jain1@tothenew.com>
2025-04-04 16:29:41 +0530

    changed the authenticate method for return type and some implementations

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/service/AuthenticationService.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/service/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.shopsavvy.shopshavvy.service;
 
 import com.shopsavvy.shopshavvy.Exception.UserNotFoundException;
+import com.shopsavvy.shopshavvy.dto.LoginResponseDTO;
 import com.shopsavvy.shopshavvy.dto.UserLoginDTO;
 import com.shopsavvy.shopshavvy.model.token.AuthToken;
 import com.shopsavvy.shopshavvy.model.token.TokenType;
@@ -10,7 +11,12 @@ import com.shopsavvy.shopshavvy.repository.UserRepository;
 import com.shopsavvy.shopshavvy.securityConfigurations.UserDetailsImpl;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthenticationService {
@@ -18,16 +24,19 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final AuthTokenRepository authTokenRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public AuthenticationService(UserRepository userRepository,
-                                 JwtService jwtService, AuthTokenRepository authTokenRepository){
+                                 JwtService jwtService, AuthTokenRepository authTokenRepository,
+                                 PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.jwtService = jwtService;
         this.authTokenRepository = authTokenRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public String authenticate(UserLoginDTO userLoginDTO) {
+    public LoginResponseDTO authenticate(UserLoginDTO userLoginDTO) {
         if (!userRepository.existsByEmail(userLoginDTO.getEmail())) {
             throw new UserNotFoundException("User not found");
         }
@@ -37,21 +46,51 @@ public class AuthenticationService {
             throw new RuntimeException("Account not verified. Please verify your account.");
         }
 
+        if (user.isLocked()) {
+            throw new RuntimeException("Account is locked. Please contact support.");
+        }
+
+        if(user.isCredentialsExpired()){
+            throw new RuntimeException("Your password has been expired.");
+        }
+
+
+        if (!passwordEncoder.matches(userLoginDTO.getPassword(), user.getPassword())) {
+            user.setInvalidAttemptCount(user.getInvalidAttemptCount() + 1);
+            if (user.getInvalidAttemptCount() >= 3) {
+                user.setLocked(true);
+            }
+            userRepository.save(user);
+            throw new BadCredentialsException("Invalid credentials. Please try again.");
+        }
 
         UserDetailsImpl userDetails = new UserDetailsImpl(user);
 
-        String token = jwtService.generateToken(userDetails, "access");
+        String accessToken = jwtService.generateToken(userDetails, "access");
+        String refreshToken = jwtService.generateToken(userDetails,"refresh");
 
-        Claims claims = jwtService.extractAllClaims(token);
+        Claims claimsForAccessToken = jwtService.extractAllClaims(accessToken);
+        Claims claimsForRefreshToken = jwtService.extractAllClaims(refreshToken);
 
-        AuthToken authToken = new AuthToken();
-        authToken.setUserEmail(userLoginDTO.getEmail());
-        authToken.setToken(token);
-        authToken.setTokenType(TokenType.ACCESS);
-        authToken.setExpirationTime(claims.getExpiration());
-        authTokenRepository.save(authToken);
+        AuthToken authTokenForAccess = new AuthToken();
+        authTokenForAccess.setUserEmail(userLoginDTO.getEmail());
+        authTokenForAccess.setToken(accessToken);
+        authTokenForAccess.setTokenType(TokenType.ACCESS);
+        authTokenForAccess.setExpirationTime(claimsForAccessToken.getExpiration());
+        authTokenRepository.save(authTokenForAccess);
 
-        return token;
+        AuthToken refreshAuthToken = new AuthToken();
+        refreshAuthToken.setUserEmail(userLoginDTO.getEmail());
+        refreshAuthToken.setToken(refreshToken);
+        refreshAuthToken.setTokenType(TokenType.REFRESH);
+        refreshAuthToken.setExpirationTime(claimsForRefreshToken.getExpiration());
+        authTokenRepository.save(refreshAuthToken);
+
+        Set<String> roles = user.getRoles().stream()
+                .map(role -> role.getAuthority())
+                .collect(Collectors.toSet());
+
+        return new LoginResponseDTO(accessToken, refreshToken, userLoginDTO.getEmail(), roles);
     }
 
 }
