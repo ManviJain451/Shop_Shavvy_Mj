271e4a196ecfd0c0cebd03bf441504a580e149a4
manvi <manvi.jain1@tothenew.com>
2025-04-04 16:21:56 +0530

    Created a dto for giving response body of authenticate method

---
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/dto/LoginResponseDTO.java
@@ -0,0 +1,17 @@
+package com.shopsavvy.shopshavvy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String accessToken;
+    private String refreshToken;
+    private String email;
+    private Set<String> role;
+}
