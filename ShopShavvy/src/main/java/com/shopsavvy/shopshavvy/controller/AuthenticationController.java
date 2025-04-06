8fb68c1f5fffc302c147f119419eae39462a91e5
manvi <manvi.jain1@tothenew.com>
2025-04-07 02:08:24 +0530

    Added endpoints for resetPassowrd and forgotPassword

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/controller/AuthenticationController.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/controller/AuthenticationController.java
@@ -3,14 +3,12 @@ package com.shopsavvy.shopshavvy.controller;
 import com.shopsavvy.shopshavvy.Exception.AlreadyActivatedException;
 import com.shopsavvy.shopshavvy.Exception.InvalidTokenException;
 import com.shopsavvy.shopshavvy.Exception.UserNotFoundException;
-import com.shopsavvy.shopshavvy.dto.CustomerRegistrationDTO;
-import com.shopsavvy.shopshavvy.dto.LoginResponseDTO;
-import com.shopsavvy.shopshavvy.dto.SellerRegistrationDTO;
-import com.shopsavvy.shopshavvy.dto.UserLoginDTO;
+import com.shopsavvy.shopshavvy.dto.*;
 import com.shopsavvy.shopshavvy.service.AuthenticationService;
 import com.shopsavvy.shopshavvy.service.CustomerAuthenticationService;
 import com.shopsavvy.shopshavvy.service.JwtService;
 import com.shopsavvy.shopshavvy.service.SellerAuthenticationService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -64,12 +62,12 @@ public class AuthenticationController {
     }
 
     @PostMapping("/resend-ActivationLink/customer")
-    public ResponseEntity<String> resendActivationLink(@RequestParam String email) throws Exception {
+    public ResponseEntity<String> resendActivationLink(@Valid @RequestParam String email) throws Exception {
         return customerAuthenticationService.resendActivationLink(email);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> authenticate(@RequestBody UserLoginDTO userLoginDTO){
+    public ResponseEntity<LoginResponseDTO> authenticate(@Valid @RequestBody UserLoginDTO userLoginDTO){
         LoginResponseDTO loginResponseDTO = authenticationService.authenticate(userLoginDTO);
         return ResponseEntity.ok().body(loginResponseDTO);
     }
@@ -79,6 +77,16 @@ public class AuthenticationController {
     public ResponseEntity<String> refreshToken(@RequestParam String refreshToken) {
         String newAccessToken = customerAuthenticationService.refreshToken(refreshToken);
         return ResponseEntity.ok().body(newAccessToken);
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<String> forgotPassword(@Valid @RequestParam String email) throws MessagingException {
+        return authenticationService.forgotPassword(email);
+    }
+
+    @PutMapping("/reset-password")
+    public ResponseEntity<ResetPasswordResponseDTO> resetPassword(@RequestParam String token, @Valid @RequestParam String password, @RequestParam String confirmPassword) throws MessagingException {
+        return authenticationService.resetPassword(token, password, confirmPassword);
     }
 
 }
