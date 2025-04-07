f956f9b7238119287b7b7660aa911474b74aa832
manvi <manvi.jain1@tothenew.com>
2025-04-08 02:21:21 +0530

    Added a entity that stores access token and its corresponding refresh token because we need to maintain login of user on multiple devices

---
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/model/token/AccessRefreshToken.java
@@ -0,0 +1,22 @@
+package com.shopsavvy.shopshavvy.model.token;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AccessRefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accessToken;
+    private String refreshToken;
+    private String userEmail;
+}
