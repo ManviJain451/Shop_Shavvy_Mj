70e384f6e381d7264135eccae53c095207f0a791
manvi <manvi.jain1@tothenew.com>
2025-04-11 15:37:58 +0530

    renamed my exception class

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/exception/AlreadyDeactivatedException.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/exception/AlreadyDeactivatedException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.FORBIDDEN)
-public class DeactivatedAccountException extends RuntimeException {
-    public DeactivatedAccountException(String message) {
+public class AlreadyDeactivatedException extends RuntimeException {
+    public AlreadyDeactivatedException(String message) {
         super(message);
     }
 }
