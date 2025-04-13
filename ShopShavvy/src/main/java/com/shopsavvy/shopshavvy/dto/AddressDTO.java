0680859c19ba6e68f3a0df56c6ca395b98fe0a8d
manvi <manvi.jain1@tothenew.com>
2025-04-14 02:36:25 +0530

    Renamed dto to generalise its use

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/dto/AddressDTO.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/dto/AddressDTO.java
@@ -1,11 +1,15 @@
 package com.shopsavvy.shopshavvy.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class AddressUpdateDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
     private String city;
 
     private String state;
