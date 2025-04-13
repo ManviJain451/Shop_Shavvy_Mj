1f6d91e7d0be8976729027d3910910f1426e5310
manvi <manvi.jain1@tothenew.com>
2025-04-13 22:52:19 +0530

    added endpoints to upload, update and delete profile picture

---
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.shopsavvy.shopshavvy.controller;
+
+import com.shopsavvy.shopshavvy.service.FileStorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/shop-shavvy/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final FileStorageService fileStorageService;
+
+    @PostMapping("/{userId}/upload-photo")
+    public ResponseEntity<String> uploadUserPhoto(@PathVariable String userId, @RequestParam("file") MultipartFile file) {
+        try {
+            String message = fileStorageService.saveOrUpdateUserPhoto(userId, file);
+            return ResponseEntity.ok(message);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload photo.");
+        }
+    }
+
+    @DeleteMapping("/{userId}/delete-photo")
+    public ResponseEntity<String> deleteProfilePhoto(@PathVariable String userId) throws IOException {
+        fileStorageService.deleteUserPhoto(userId);
+        return ResponseEntity.ok("Profile photo deleted successfully.");
+
+    }
+}
