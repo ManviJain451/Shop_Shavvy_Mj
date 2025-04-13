139d041f68ced7a3718b13675a01727208eec346
manvi <manvi.jain1@tothenew.com>
2025-04-14 02:39:36 +0530

    Added endpoints for customer to view profile, update profile, add , update and delete address

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/controller/CustomerController.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/controller/CustomerController.java
@@ -1,9 +1,6 @@
 package com.shopsavvy.shopshavvy.controller;
 
-import com.shopsavvy.shopshavvy.dto.CustomerResponseDTO;
-import com.shopsavvy.shopshavvy.dto.CustomerViewProfileDTO;
-import com.shopsavvy.shopshavvy.dto.LoginResponseDTO;
-import com.shopsavvy.shopshavvy.dto.UserLoginDTO;
+import com.shopsavvy.shopshavvy.dto.*;
 import com.shopsavvy.shopshavvy.service.AuthenticationService;
 import com.shopsavvy.shopshavvy.service.CustomerService;
 import jakarta.mail.MessagingException;
@@ -11,9 +8,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,5 +37,46 @@ public class CustomerController {
     public ResponseEntity<CustomerViewProfileDTO> getProfile(@RequestParam String accessToken) {
         CustomerViewProfileDTO customerProfile = customerService.getCustomerProfile(accessToken);
         return ResponseEntity.ok(customerProfile);
+    }
+
+    @GetMapping("/view-addresses")
+    public ResponseEntity<List<AddressDTO>> getAddresses(@RequestParam String accessToken) {
+        List<AddressDTO> addresses = customerService.getCustomerAddresses(accessToken);
+        return ResponseEntity.ok(addresses);
+    }
+
+    @PatchMapping("/update-profile")
+    public ResponseEntity<?> updateProfile(@RequestParam String accessToken,
+            @Valid @ModelAttribute CustomerUpdateProfileDTO customerUpdateProfileDTO) {
+
+        customerService.updateCustomerProfile(accessToken, customerUpdateProfileDTO);
+        return ResponseEntity.ok("Profile updated successfully.");
+    }
+
+    @PostMapping("/add-address")
+    public ResponseEntity<?> addAddress(
+            @RequestParam String accessToken,
+            @Valid @ModelAttribute AddressDTO addressDTO) {
+
+        customerService.addCustomerAddress(accessToken, addressDTO);
+        return ResponseEntity.ok("Address added successfully.");
+    }
+
+    @DeleteMapping("/delete-address")
+    public ResponseEntity<String> deleteAddress(
+            @RequestParam String accessToken,
+            @RequestParam String addressId) {
+
+        customerService.deleteCustomerAddress(accessToken, addressId);
+        return ResponseEntity.ok("Address deleted successfully.");
+    }
+
+    @PatchMapping("/update-address")
+    public ResponseEntity<String> updateCustomerAddress(@RequestParam String accessToken,
+            @RequestParam String addressId,
+            @Valid @ModelAttribute CustomerAddressDTO customerAddressDTO) {
+
+        customerService.updateCustomerAddress(accessToken, addressId, customerAddressDTO);
+        return ResponseEntity.ok("Address updated successfully.");
     }
 }
