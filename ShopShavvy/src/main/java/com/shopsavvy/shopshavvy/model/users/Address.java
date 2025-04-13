9c7342b3b69e9f46b99906795c2111f8e44bd085
manvi <manvi.jain1@tothenew.com>
2025-04-14 02:41:35 +0530

    Changed the data type of address id from long to string and generation type from identity to uuid

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/model/users/Address.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/model/users/Address.java
@@ -12,8 +12,8 @@ import lombok.*;
 public class Address {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
     private String city;
 
