448a1f17260e3c8a66b3c8b367685fe441380355
manvi <manvi.jain1@tothenew.com>
2025-04-14 15:58:42 +0530

    Reduce redundancy by using passwordDTO

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/dto/ResetPasswordResponseDTO.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/dto/ResetPasswordResponseDTO.java
@@ -1,28 +1,19 @@
-// ResetPasswordResponseDTO.java
 package com.shopsavvy.shopshavvy.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class ResetPasswordResponseDTO {
+@NoArgsConstructor
+public class ResetPasswordResponseDTO extends PasswordDTO {
     private String resetPasswordToken;
 
-    @NotBlank(message = "Password is mandatory")
-    @Size(min = 8, max = 15, message = "Password must be between 8 and 15 characters long")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
-            message = "Password must contain at least one lowercase letter, one uppercase letter, one special character, and one number")
-    private String password;
-
-    @NotBlank(message = "Confirm Password is mandatory")
-    @Size(min = 8, max = 15, message = "Password must be between 8 and 15 characters long")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
-            message = "Password must contain at least one lowercase letter, one uppercase letter, one special character, and one number")
-    private String confirmPassword;
+    public ResetPasswordResponseDTO(String resetPasswordToken, String password, String confirmPassword) {
+        super();
+        this.resetPasswordToken = resetPasswordToken;
+    }
 }
+
