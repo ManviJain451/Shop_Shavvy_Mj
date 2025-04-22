6851526e0c6357a610dc53123381313973a2db36
manvi <manvi.jain1@tothenew.com>
2025-04-22 17:09:47 +0530

    Removed password expiring scheduler

--- ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/service/ScheduledService.java
+++ ShopShavvy/src/main/java/com/shopsavvy/shopshavvy/service/ScheduledService.java
@@ -7,10 +7,6 @@ import com.shopsavvy.shopshavvy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
 
@@ -37,16 +33,4 @@ public class ScheduledService {
         Date now = new Date();
         authTokenRepository.deleteByExpirationTimeBefore(now);
     }
-
-//    @Scheduled(cron = "0 0 */1 * * *")
-//    @Transactional
-//    public void checkPasswordExpiration() {
-//        LocalDateTime expirationThreshold = LocalDateTime.now().minus(15, ChronoUnit.MINUTES);
-//        List<User> usersWithExpiredPasswords = userRepository.findByPasswordLastUpdateDateBeforeAndExpiredFalse(expirationThreshold);
-//
-//        usersWithExpiredPasswords.forEach(user -> {
-//            user.setExpired(true);
-//            userRepository.save(user);
-//        });
-//    }
 }
